86ad56fcd881f053c838dd02d9661a57b8fce046
AriukCS1A <104846136+AriukCS1A@users.noreply.github.com>
2023-04-26 10:49:40 +0800

    Add files via upload

--- index.js
+++ index.js
@@ -1,11 +1,29 @@
-function test (){
-    // oruulsan utgiig avah
-    let fname  = document.getElementById("fname").value;
-    let lname = document.getElementById ("lname").value;
-    let uls = document.getElementById ("country").value;
-    alert("first name is: " + fname +"\nlast name is: "+ lname + "\nCountry: " + uls);
-    // hevleh uildel
-    document.getElementById ("print").innerHTML = 
-    "first name is: " + fname +"\nlast name is: "+ lname + "\nCountry: " + uls
+const dino = document.getElementById ("player");
+function usreh () {
+    if (dino.classList != "jump"){
+        dino.classList.add ("jump");
+        setTimeout (function(){
+            dino.classList.remove("jump");
+        }, 500);
+    }
 }
-
+document.addEventListener ("keydown", function(event){
+    if (event.keyCode == 32){
+        usreh();
+    }
+});
+const game = document.getElementById ("game")
+const cactus = document.getElementById ("cactus");
+let isAlive = setInterval (function(){
+    let dinoTop = parseInt(window.getComputedStyle(dino).getPropertyValue ("top"));
+    let cactusLeft = parseInt (window.getComputedStyle(cactus).getPropertyValue ("left"));
+    if (cactusLeft < 50 && cactusLeft > 0 && dinoTop >= 140){
+        isAlive = false;
+        if (confirm("Do you want to start?")){
+            location.reload ();
+            isAlive = true;
+        }else{
+            game.remove();
+        }
+    }
+}, 10);
